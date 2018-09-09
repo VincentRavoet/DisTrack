6f88fca6387d1a296117e4d6e620b92f1de4c556
VincentRavoet <vincentravoet@hotmail.com>
2018-09-09 16:01:55 +0200

    Functioneel: meest recente release voor artiest werkt!

--- src/releasesActions.js
+++ src/releasesActions.js
@@ -1,7 +1,8 @@
 var Discogs = require("disconnect").Client;
-var async = require("async");
 
-var col = new Discogs().user().collection();
+var client = new Discogs();
+var col = client.user().collection();
+var db = client.database();
 
 module.exports.fetchNewestReleaseFromArtistsForUsername = (username) => {
     return col.getReleases(username, 0, { page: 1, per_page: 1000 })
@@ -14,19 +15,28 @@ fetchReleasesForArtists = (releases) => {
     return new Promise((resolve, reject) => {
         if (releases) {
             var artistReleaseCollection = {};
-
-            releases.forEach(release => {
+            var relSlice = releases.slice(1, 2);
+            relSlice.forEach(release => {
                 var releaseArtists = release.basic_information.artists;
 
                 releaseArtists.forEach(artist => {
-                    var releaseArtistName = artist.name;
+                    var releaseArtistID = artist.id;
                     var releaseTitle = release.basic_information.title;
-
-                    if (!artistReleaseCollection[releaseArtistName]) {
-                        artistReleaseCollection[releaseArtistName] = [];
+                    var params = {
+                        sort: 'year',
+                        sort_order: 'desc',
                     }
 
-                    artistReleaseCollection[releaseArtistName].push(releaseTitle);
+                    // Deze functie haalt de meest recente release voor een artist uit Discogs.
+                    db.getArtistReleases(5003, params, artistReleasesCallback);
+
+
+
+                    if (!artistReleaseCollection[releaseArtistID]) {
+                        artistReleaseCollection[releaseArtistID] = [];
+                    }
+
+                    artistReleaseCollection[releaseArtistID].push(releaseTitle);
                 })
             });
 
@@ -36,4 +46,8 @@ fetchReleasesForArtists = (releases) => {
             return;
         }
     });
+};
+
+var artistReleasesCallback = function (err, data, rateLimit) {
+    console.log(data.releases[0]);
 }
